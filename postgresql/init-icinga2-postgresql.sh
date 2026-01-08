525bd478a060f81f9ec9411627db894b65aa9f4e
agougo@gmail.com <agougo@gmail.com>
2026-01-08 15:32:14 +0100

    Tweaked Postgresql and added icinga2 container

--- postgresql/init-icinga2-postgresql.sh
+++ postgresql/init-icinga2-postgresql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+
+set -euo pipefail
+trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
 
 echo "=========================================="
 echo "Starting Icinga PostgreSQL initialization"
